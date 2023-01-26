89e5a8c4ce252a685ba2b2564f3852d00db531e8
Jordan Girardi <jordangirardi000@gmail.com>
2023-01-26 13:59:06 -0300

    Add checkbox component.

---
+++ src/app/components/checkbox/checkbox.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-checkbox',
+  templateUrl: './checkbox.component.html',
+  styleUrls: ['./checkbox.component.scss'],
+})
+export class CheckboxComponent {
+  @Input() label!: string;
+  @Output() onCheckboxChange: EventEmitter<any> = new EventEmitter();
+
+  protected handleChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.onCheckboxChange.emit(input.checked);
+  }
+}
