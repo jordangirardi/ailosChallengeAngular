d173729a73e869226e695cb2d0a734f5fe5be4fa
Jordan Girardi <jordangirardi000@gmail.com>
2023-01-26 13:54:25 -0300

    Add models, interfaces and enums.

---
+++ src/app/interfaces/navItem.ts
@@ -0,0 +1,4 @@
+export interface NavItem {
+  description: string;
+  icon: string;
+}
---
+++ src/app/models/client.ts
@@ -0,0 +1,26 @@
+export class Client {
+  nome: string = '';
+  apelido: string = '';
+  dataNascimento: string = '';
+  capacidadeCivil!: number;
+  dataEmancipacao: string = '';
+  estadoCivil!: number;
+  isPCD!: number;
+  necessidade: string = '';
+  tipoNacionalidade!: number;
+  nacionalidade!: number;
+  naturalidade!: number;
+  sexo!: number;
+  nomeMae: string = '';
+  nomePai: string = '';
+  cpf: string = '';
+  situacaoCpf!: number;
+  dataConsultaCpf: string = '';
+  documentoIdentidade!: number;
+  numeroDocumentoIdentidade: string = '';
+  orgaoEmissor!: number;
+  ufEmissor!: number;
+  dataEmissao: string = '';
+
+  pictureSource: string = '';
+}
