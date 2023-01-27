f66a1cbad9ba809309fe3897cc41480614ae3171
Jordan Girardi <jordangirardi000@gmail.com>
2023-01-27 15:42:26 -0300

    Added thumbnail in file upload component.

--- src/app/components/file-upload/file-upload.component.ts
+++ src/app/components/file-upload/file-upload.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DocumentType } from 'src/app/interfaces/document-type';
+import { DocumentTypeComponent } from '../dialogs/document-type/document-type.component';
 
 @Component({
   selector: 'app-file-upload',
@@ -7,7 +10,43 @@ import { Component, Input } from '@angular/core';
 })
 export class FileUploadComponent {
   @Input() isNecessary: boolean = true;
+  @Input() isValidateDocumentType: boolean = true;
+
+  @ViewChild('fileInput') fileInput!: ElementRef;
+
+  protected imgUploadedSrc: string = '';
   protected necessaryText: string = this.isNecessary
     ? 'Obrigatório'
     : 'Opcional';
+
+  constructor(private dialog: MatDialog) {}
+
+  protected confirmDocumentType(): void {
+    if (this.isValidateDocumentType) {
+      const dialogRef = this.dialog.open(DocumentTypeComponent, {
+        data: { documentType: null },
+      });
+
+      dialogRef.afterClosed().subscribe((result) => {
+        console.log(result); //tipo de doc escolhido
+        if (result.documentType !== null) {
+          this.fileInput.nativeElement.click();
+        }
+      });
+    } else {
+      this.fileInput.nativeElement.click();
+    }
+  }
+
+  protected handleFileChange(event: any): void {
+    const file = event.target.files.item(0);
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      if (reader.result) {
+        this.imgUploadedSrc = reader.result.toString();
+      }
+    };
+  }
 }
